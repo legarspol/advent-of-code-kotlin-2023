4397bae430f190da6b4e447eed799c3ac138aa9d
Pol <polegars@microsoft.com>
2023-12-03 12:15:25 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,66 @@
+fun startWith(s: String): String? {
+    if (s[0] in '0'..'9')
+        return s[0].toString()
+    val list =
+        listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    for ((i, value) in list.withIndex()) {
+        if (s.startsWith(value)) {
+            return (i + 1).toString();
+        }
+    }
+    return null;
+
+}
+
+fun endWith(s: String): String? {
+    if (s[s.length-1] in '0'..'9')
+        return s[s.length-1].toString()
+    val list =
+        listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    for ((i, value) in list.withIndex()) {
+        if (s.endsWith(value)) {
+            return (i + 1).toString();
+        }
+    }
+    return null;
+
+}
+
+fun getNb(s: String): String {
+
+    var nb = "";
+    for (i in s.indices) {
+        //                println(s[i])
+        val oui = startWith(s.substring(i))
+        if (oui != null) {
+            nb += oui
+            break
+        }
+    }
+    for (i in s.length downTo 0) {
+        //                println(s[i])
+        val oui = endWith(s.substring(0, i))
+        if (oui != null) {
+            nb += oui
+            break
+        }
+    }
+    println(nb)
+    return nb
+}
+
+
 fun main() {
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var out = 0;
+        for (s in input) {
+
+            var nb = getNb(s)
+            out += nb.toInt()
+        }
+        return out
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +69,11 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testResult = part1(testInput)
+    println("Result test: $testResult")
+    check(testResult == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("Result part1: ${part1(input)}")
+    println("Result part2: ${part2(input)}")
 }
